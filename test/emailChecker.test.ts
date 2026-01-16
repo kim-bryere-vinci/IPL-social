962c898fd91d5c77b8c15b63dfc1aa1322247307
kim.bryere <kim.bryere@student.vinci.be>
2026-01-16 16:25:00 +0100

    add test: Il doit contenir au moins un point dans le nom de domaine

--- test/emailChecker.test.ts
+++ test/emailChecker.test.ts
@@ -3,6 +3,11 @@ import {isValidEmail} from "../src/emailChecker"
 describe("email checker", () => {
     test("Il doit contenir au moins @", () => {
         expect(isValidEmail("kim.bryerestudent.vinci.be")).toBe(false);
-    })
+    }),
+    
+    test("Il doit contenir au moins un point dans le nom de domaine", () => {
+        expect(isValidEmail("kimbryerestudentvincibe")).toBe(false);
+    }),
+
 
 })
