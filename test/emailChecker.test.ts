9e9824c40f1ca4cb725d1e9ee07c5b2db78dc91e
kim.bryere <kim.bryere@student.vinci.be>
2026-01-16 16:25:23 +0100

    add test : Il ne peut contenir aucun espace

--- test/emailChecker.test.ts
+++ test/emailChecker.test.ts
@@ -4,10 +4,12 @@ describe("email checker", () => {
     test("Il doit contenir au moins @", () => {
         expect(isValidEmail("kim.bryerestudent.vinci.be")).toBe(false);
     }),
-    
+
     test("Il doit contenir au moins un point dans le nom de domaine", () => {
         expect(isValidEmail("kimbryerestudentvincibe")).toBe(false);
     }),
 
-
+    test("Il ne peut contenir aucun espace", () => {
+        expect(isValidEmail("kimbryerestudentvi    ncibe")).toBe(false);
+    }),
 })
