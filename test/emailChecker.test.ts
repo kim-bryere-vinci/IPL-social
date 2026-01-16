9fd7fa5c30d8028b7100d70b5630352d5fa1e65b
kim.bryere <kim.bryere@student.vinci.be>
2026-01-16 16:32:09 +0100

    add function isEmailValid

--- test/emailChecker.test.ts
+++ test/emailChecker.test.ts
@@ -3,10 +3,12 @@ import {isValidEmail} from "../src/emailChecker"
 describe("email checker", () => {
     test("Il doit contenir au moins @", () => {
         expect(isValidEmail("kim.bryerestudent.vinci.be")).toBe(false);
+        expect(isValidEmail("kim.bryerest@@@udent.vinci.be")).toBe(false);
     }),
 
     test("Il doit contenir au moins un point dans le nom de domaine", () => {
         expect(isValidEmail("kimbryerestudentvincibe")).toBe(false);
+        expect(isValidEmail("kimbryerestudentvinci...be")).toBe(false);
     }),
 
     test("Il ne peut contenir aucun espace", () => {
